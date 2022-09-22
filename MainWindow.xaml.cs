ad69606d87beb6924c035ffa602047a37669cfbd
Aleksei <s2001175@edu.bc.fi>
2022-09-22 11:03:16 +0300

    Ristinolla

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,68 @@ namespace ristinolla
         {
             InitializeComponent();
         }
+
+        string currentPlayer = "X";
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Mark(btn0);
+        }
+
+        private void Mark(Button btn)
+        {
+            if (btn.Content == null)
+            {
+                btn.Content = currentPlayer;
+                if (currentPlayer == "X")
+                {
+                    currentPlayer = "O";
+                }
+                else
+                {
+                    currentPlayer = "X";
+                }
+            }
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            Mark(btn1);
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            Mark(btn2);
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            Mark(btn3);
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            Mark(btn4);
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            Mark(btn5);
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            Mark(btn6);
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            Mark(btn7);
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            Mark(btn8);
+        }
     }
 }
